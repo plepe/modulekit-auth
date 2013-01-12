1dc2aac9adf896e0da6b0fa12648a7200dbcf132
Stephan Plepelits <skunk@xover.mud.at>
2013-01-12 10:28:40 +0100

    Auth: implement domains() and authenticate()

--- inc/Auth.php
+++ inc/Auth.php
@@ -16,7 +16,45 @@ class Auth {
     return $this->current_user;
   }
 
+  function domains() {
+    if(isset($this->domains))
+      return $this->domains;
+
+    $this->domains=array();
+
+    if((!isset($this->config['domains']))&&
+       (!is_array($this->config['domains'])))
+      return false;
+
+    foreach($this->config['domains'] as $domain=>$domain_config) {
+      $class="Auth_".$domain_config['type'];
+      modulekit_load(array($domain_config['type']));
+
+      if(class_exists($class))
+        $this->domains[$domain]=new $class($domain_config);
+    }
+
+    return $this->domains;
+  }
+
   function authenticate($username, $password, $options=array()) {
+    $errors=array();
+
+    foreach($this->domains() as $domain=>$domain_object) {
+      $result=$domain_object->authenticate($username, $password, $options);
+
+      if(is_array($result)) {
+        $this->current_user=new Auth_User($username, $domain, $result);
+
+        return true;
+      }
+      elseif(is_string($result)) {
+        $errors[]="Domain '{$domain}': {$result}";
+      }
+    }
+
+    if(sizeof($errors))
+      return $errors;
     return false;
   }
 }
