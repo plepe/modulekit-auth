2ccd3dcff8307d31f944a418407b8ba5d64919ac
Stephan Plepelits <skunk@xover.mud.at>
2013-01-12 10:10:40 +0100

    Automatically load all modules for $auth_config types

--- modulekit.php
+++ modulekit.php
@@ -8,3 +8,29 @@ $include=array(
     "inc/*.php",
   ),
 );
+
+if(!function_exists("modulekit_auth_check_modules")) {
+  function modulekit_auth_check_modules() {
+    global $auth_config;
+
+    if(isset($auth_config)&&
+       is_array($auth_config)&&
+       isset($auth_config['domains'])&&
+       is_array($auth_config['domains'])) {
+
+      $load=array();
+
+      foreach($auth_config['domains'] as $c) {
+        if(isset($c['type'])) {
+          $module_name="modulekit-auth-{$c['type']}";
+
+          if(!in_array($module_name, $load))
+            $load[]=$module_name;
+        }
+      }
+    }
+
+    return $load;
+  }
+}
+$load=modulekit_auth_check_modules();
