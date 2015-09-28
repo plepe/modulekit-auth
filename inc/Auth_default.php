f28734161aedaef5a500b8e00e49a29da9430a8e
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2015-09-28 12:00:58 +0200

    Auth default: documentation

--- inc/Auth_default.php
+++ inc/Auth_default.php
@@ -4,6 +4,40 @@ class Auth_default {
     $this->config = $config;
   }
 
+  /**
+   * Tries to authenticate with username/password to the domain
+   * @param string Username
+   * @param string Password
+   * @param mixed[] Reserved for future use
+   * @return false|string|Auth_User false: not found, string: an error occured
+   *   - Error message, Auth_User: user object of successful authentication
+   */
+  function authenticate($username, $password, $options=array()) {
+    return false;
+  }
+
+  /**
+   * Return user object for the given username
+   * @param string Username
+   * @return null|Auth_User null: not found, Auth_User: user object
+   */
+  function get_user($username) {
+    return null;
+  }
+
+  /**
+   * Return group members for the given group name
+   * @param string Group name
+   * @return string[] List of user names
+   */
+  function group_members($group) {
+    return array();
+  }
+
+  /**
+   * Return all users in the given domain
+   * @return string[] List of user names
+   */
   function users() {
     return array();
   }
