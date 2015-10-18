375e8f49ea304f9534c4cd5bcfe5c014ad81b21e
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2015-10-18 14:25:05 +0200

    test_js.php: show userdata from JS

--- test_js.php
+++ test_js.php
@@ -34,6 +34,20 @@ if(isset($auth_result)) {
     <?php print modulekit_include_js(); /* prints all js-includes */ ?>
     <?php print modulekit_include_css(); /* prints all css-includes */ ?>
     <?php print_add_html_headers(); /* prints all css-includes */ ?>
+  <script type='text/javascript'>
+window.onload = function() {
+  var div = document.getElementById("userdata");
+
+  var ret = "";
+  ret += "ID: " + auth.current_user().id() + "\n";
+  ret += "Name: " + auth.current_user().name() + "\n";
+  ret += "Domain: " + auth.current_user().domain + "\n";
+  ret += "E-Mail: " + auth.current_user().email() + "\n";
+  ret += "Userdata: " + JSON.stringify(auth.current_user().data, null, '    ');
+
+  div.appendChild(document.createTextNode(ret));
+}
+  </script>
   </head>
   <body>
 <?php
@@ -50,8 +64,7 @@ if($error) {
     <hr/>
     <?php
 
-print "Userdata: <pre>\n";
-print_r($current_user);
+print "Userdata: <pre id='userdata'>\n";
 print "</pre><hr/>\n";
 
 print "Users:\n";
