04a121a6de69fe7d2cf4b2b6149fe0d6fa6ab583
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2015-10-21 12:24:43 +0200

    Auth Pages: when POSTing directly to page login, redirect back to referer

--- modules/auth-pages/page_login.php
+++ modules/auth-pages/page_login.php
@@ -11,6 +11,10 @@ class Page_login extends Page {
     if($auth->is_logged_in()) {
       if(isset($this->param['return']))
 	page_reload($this->param['return']);
+      elseif(isset($_SERVER['HTTP_REFERER']))
+	page_reload($_SERVER['HTTP_REFERER']);
+      else
+	page_reload(array());
 
       return "<p><a href='?{$this->param['return']}'>Continue</a>";
     }
