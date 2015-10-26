29017b26b0dac16a026bfcbcca740e5a05280009
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2015-10-26 20:41:56 +0100

    Auth Pages::Login: Bugfix, don't redirect to login page after login

--- modules/auth-pages/page_login.php
+++ modules/auth-pages/page_login.php
@@ -11,8 +11,20 @@ class Page_login extends Page {
     if($auth->is_logged_in()) {
       if(isset($this->param['return']))
 	page_reload($this->param['return']);
-      elseif(isset($_SERVER['HTTP_REFERER']))
-	page_reload($_SERVER['HTTP_REFERER']);
+      elseif(isset($_SERVER['HTTP_REFERER'])) {
+        if(strpos($_SERVER['HTTP_REFERER'], '?') !== false) {
+          $referer = substr($_SERVER['HTTP_REFERER'], strpos($_SERVER['HTTP_REFERER'], '?') + 1);
+          parse_str($referer, $referer);
+
+          if($referer['page'] == 'login')
+            page_reload(array());
+          else
+            page_reload($_SERVER['HTTP_REFERER']);
+        }
+        else {
+          page_reload($_SERVER['HTTP_REFERER']);
+        }
+      }
       else
 	page_reload(array());
 
